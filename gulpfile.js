33c4f434cf6d27eae936b82b19992763c1ed6aae
Ascen Keeprov <assenkiprov@yahoo.com>
2019-08-04 16:00:37 +0300

    Extended Gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,55 @@
 'use strict';
 
 const browserSync = require('browser-sync').create();
-const childProcess = require('child_process');
 const cssMinifier = require('cssnano');
 const cssPrefixer = require('autoprefixer');
 const cssProcessor = require('gulp-postcss');
+const deploymentManager = require('gh-pages');
 const fileCombiner = require('gulp-concat');
+const fileManager = require('fs');
 const gulp = require('gulp');
 const imageMinifier = require('gulp-imagemin');
 const jsMinifier = require('gulp-terser');
+const processManager = require('child_process');
+const sassProcessor = require('gulp-sass');
 const sourceMapper = require('gulp-sourcemaps');
-const sassCompiler = require('gulp-sass');
-const siteAssetsDir = '_assets';
-const siteFragmentsDir = '_includes';
-const siteLayoutsDir = '_layouts';
-const siteOutputDir = 'docs';
 const supportedFontTypes = '{ttf,woff?(2)}';
 const supportedImageTypes = '{gif,ico,jp?(e)g,png}';
 const supportedPageTypes = 'html';
 const supportedScriptTypes = 'js';
 const supportedStyleTypes = '?(s)css';
+const yamlParser = require('js-yaml');
+
+let config = getJekyllConfig('./_config.yml');
+
+sassProcessor.compiler = require('node-sass');
+
+function getJekyllConfig(configFilePath) {
+	try {
+		let configObj = {};
+		let jekyllConfig = yamlParser.safeLoad(fileManager.readFileSync(configFilePath, { encoding: 'utf8' }));
+		configObj.assetsPath = `./${jekyllConfig.assets_dir}`;
+		configObj.layoutsPath = `./${jekyllConfig.layouts_dir}`;
+		configObj.outputPath = jekyllConfig.destination;
+		configObj.partialsPath = `./${jekyllConfig.includes_dir}`;
+		configObj.deployUrl = `git@github.com:${jekyllConfig.repository}`;
+		return configObj;
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+function deployGithubPage() {
+	return gulp.src('./**/*')
+		.pipe(deploymentManager({
+			branch: 'master',
+			message: 'Deployed via Gulp',
+			remoteUrl: config.deployUrl
+		}));
+}
 
 function jekyllClean() {
-	return childProcess.exec('bundle exec jekyll clean', (exception, stdout, stderr) => {
+	return processManager.exec('bundle exec jekyll clean', (exception, stdout, stderr) => {
 		if (exception) {
 			console.log(exception.stack);
 			console.log(`Child process exited with code ${exception.code}`);
@@ -34,7 +61,7 @@ function jekyllClean() {
 }
 
 function jekyllBuild() {
-	return childProcess.spawn('bundle', ['exec', 'jekyll', 'build'], { shell: true, stdio: 'inherit' });
+	return processManager.spawn('bundle', ['exec', 'jekyll', 'build'], { shell: true, stdio: 'inherit' });
 }
 
 function loadAssets() {
@@ -42,37 +69,41 @@ function loadAssets() {
 }
 
 function loadFonts() {
-	return gulp.src(`./${siteAssetsDir}/fonts/**/*.${supportedFontTypes}`)
-		.pipe(gulp.dest(`./${siteOutputDir}/fonts`));
+	return gulp.src(`${config.assetsPath}/fonts/**/*.${supportedFontTypes}`)
+		.pipe(gulp.dest(`${config.outputPath}/fonts`));
 }
 
 function loadImages() {
-	return gulp.src(`./${siteAssetsDir}/images/**/*.${supportedImageTypes}`)
+	return gulp.src(`${config.assetsPath}/images/**/*.${supportedImageTypes}`)
 		.pipe(imageMinifier())
-		.pipe(gulp.dest(`./${siteOutputDir}/images`));
+		.pipe(gulp.dest(`${config.outputPath}/images`));
 }
 
 function loadScripts() {
-	return gulp.src(`./${siteAssetsDir}/scripts/**/*.${supportedScriptTypes}`)
+	return gulp.src(`${config.assetsPath}/scripts/**/*.${supportedScriptTypes}`)
 		.pipe(fileCombiner('site.min.js'))
 		.pipe(sourceMapper.init())
 		.pipe(jsMinifier())
 		.pipe(sourceMapper.write('./'))
-		.pipe(gulp.dest(`./${siteOutputDir}/scripts`));
+		.pipe(gulp.dest(`${config.outputPath}/scripts`));
 }
 
 function loadStyles() {
-	let plugins = [
-		cssMinifier(),
-		cssPrefixer()
-	];
-	return gulp.src(`./${siteAssetsDir}/styles/**/*.${supportedStyleTypes}`)
-		.pipe(sassCompiler().on('error', sassCompiler.logError))
+	return gulp.src(`${config.assetsPath}/styles/**/*.${supportedStyleTypes}`)
+		.pipe(sassProcessor({
+			indentType: 'tab',
+			indentWidth: 1,
+			linefeed: 'crlf',
+			outputStyle: 'expanded'
+		}).on('error', sassProcessor.logError))
 		.pipe(sourceMapper.init())
-		.pipe(cssProcessor(plugins))
+		.pipe(cssProcessor([
+			cssMinifier(),
+			cssPrefixer()
+		]))
 		.pipe(fileCombiner('site.min.css'))
 		.pipe(sourceMapper.write('./'))
-		.pipe(gulp.dest(`./${siteOutputDir}/styles`))
+		.pipe(gulp.dest(`${config.outputPath}/styles`))
 		.pipe(browserSync.stream({ match: '**/*.css' }));
 }
 
@@ -80,14 +111,14 @@ function watchForChanges() {
 	browserSync.init({
 		notify: false,
 		port: 8080,
-		server: { baseDir: `./${siteOutputDir}/` },
+		server: { baseDir: `${config.outputPath}/` },
 		ui: { port: 9090 }
 	});
 
 	let pageSource = [
 		`./*.${supportedPageTypes}`,
-		`./${siteFragmentsDir}/**/*.${supportedPageTypes}`,
-		`./${siteLayoutsDir}/**/*.${supportedPageTypes}`
+		`${config.partialsPath}/**/*.${supportedPageTypes}`,
+		`${config.layoutsPath}/**/*.${supportedPageTypes}`
 	];
 	let jekyllRebuildSequence = gulp.series(jekyllBuild, loadAssets(), browserSync.reload);
 	gulp.watch(pageSource).on('add', jekyllRebuildSequence);
@@ -95,25 +126,25 @@ function watchForChanges() {
 	gulp.watch(pageSource).on('unlink', jekyllRebuildSequence);
 
 	let fontsReloadSequence = gulp.series(loadFonts, browserSync.reload);
-	let fontSource = `./${siteAssetsDir}/fonts/**/*.${supportedFontTypes}`;
+	let fontSource = `${config.assetsPath}/fonts/**/*.${supportedFontTypes}`;
 	gulp.watch(fontSource).on('add', fontsReloadSequence);
 	gulp.watch(fontSource).on('change', fontsReloadSequence);
 	gulp.watch(fontSource).on('unlink', fontsReloadSequence);
 
 	let imagesReloadSequence = gulp.series(loadImages, browserSync.reload);
-	let imageSource = `./${siteAssetsDir}/images/**/*.${supportedImageTypes}`;
+	let imageSource = `${config.assetsPath}/images/**/*.${supportedImageTypes}`;
 	gulp.watch(imageSource).on('add', imagesReloadSequence);
 	gulp.watch(imageSource).on('change', imagesReloadSequence);
 	gulp.watch(imageSource).on('unlink', imagesReloadSequence);
 
 	let scriptsReloadSequence = gulp.series(loadScripts, browserSync.reload);
-	let scriptSource = `./${siteAssetsDir}/scripts/**/*.${supportedScriptTypes}`;
+	let scriptSource = `${config.assetsPath}/scripts/**/*.${supportedScriptTypes}`;
 	gulp.watch(scriptSource).on('add', scriptsReloadSequence);
 	gulp.watch(scriptSource).on('change', scriptsReloadSequence);
 	gulp.watch(scriptSource).on('unlink', scriptsReloadSequence);
 
 	let stylesReloadSequence = gulp.series(loadStyles, browserSync.reload);
-	let styleSource = `./${siteAssetsDir}/styles/**/*.${supportedStyleTypes}`;
+	let styleSource = `${config.assetsPath}/styles/**/*.${supportedStyleTypes}`;
 	gulp.watch(styleSource).on('add', stylesReloadSequence);
 	gulp.watch(styleSource).on('change', stylesReloadSequence);
 	gulp.watch(styleSource).on('unlink', stylesReloadSequence);
@@ -121,3 +152,4 @@ function watchForChanges() {
 
 exports.clean = jekyllClean;
 exports.default = gulp.series(jekyllClean, jekyllBuild, loadAssets(), watchForChanges);
+exports.deploy = deployGithubPage;
