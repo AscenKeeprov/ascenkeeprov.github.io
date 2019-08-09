15dbf5a205e1036f9a3f70fb058ddfb7cf7577d4
Ascen Keeprov <assenkiprov@yahoo.com>
2019-08-09 19:54:53 +0300

    Amended styling script for Biography page to take <details> interaction into account

--- _assets/scripts/biography.js
+++ _assets/scripts/biography.js
@@ -1,6 +1,6 @@
 const faxPaperStripClassName = 'fax-paper-strip';
 
-let faxPaperStripHolesCount = 36;
+let faxPaperStripHolesCount = 38;
 let faxPaperStrips = Array.from(document.getElementsByClassName(faxPaperStripClassName));
 
 function createFaxPaperStripHole() {
@@ -19,16 +19,24 @@ function populateFaxPaperStrip(faxPaperStrip) {
 }
 
 function populateFaxPaperStrips() {
-	let contentHeight = document.getElementsByClassName('fax-paper-content')[0].scrollHeight;
-	let computedStyle = window.getComputedStyle(document.getElementsByTagName('html')[0]);
-	let fontSize = parseFloat(computedStyle.fontSize);
-	let lineHeightMultiplier = parseFloat(computedStyle.lineHeight) / fontSize;
-	faxPaperStripHolesCount = Math.round(contentHeight / fontSize / lineHeightMultiplier);
-	faxPaperStrips.forEach(fps => populateFaxPaperStrip(fps));
+	if (faxPaperStrips.length) {
+		let content = Array.from(document.getElementsByClassName('fax-paper-content')[0].children);
+		let contentHeight = content.map(c => c.scrollHeight).reduce((c1, c2) => c1 + c2, 0);
+		let computedStyle = window.getComputedStyle(document.getElementsByTagName('html')[0]);
+		let fontSize = parseFloat(computedStyle.fontSize);
+		let lineHeightMultiplier = (parseFloat(computedStyle.lineHeight) / fontSize) * 0.74;
+		faxPaperStripHolesCount = Math.round(contentHeight / fontSize / lineHeightMultiplier);
+		console.log(faxPaperStripHolesCount);
+		faxPaperStrips.forEach(fps => populateFaxPaperStrip(fps));
+	}
 }
 
 Array.from(document.querySelectorAll('li[data-order]')).forEach(listItem => {
 	listItem.style.order = listItem.dataset.order;
+});
+
+Array.from(document.getElementsByTagName('details')).forEach(d => {
+	d.addEventListener('toggle', populateFaxPaperStrips);
 });
 
 window.addEventListener('load', populateFaxPaperStrips);
